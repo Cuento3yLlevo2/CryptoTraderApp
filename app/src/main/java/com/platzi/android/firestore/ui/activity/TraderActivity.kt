e8547175d2faaa4afc95b9860c5964ff22cb2240
Cuento3yLlevo2 <mhermoso1993@gmail.com>
2021-07-29 16:30:20 +0200

    first commit

---
+++ app/src/main/java/com/platzi/android/firestore/ui/activity/TraderActivity.kt
@@ -0,0 +1,37 @@
+package com.platzi.android.firestore.ui.activity
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.platzi.android.firestore.R
+
+
+/**
+ * @author Santiago Carrillo
+ * 2/14/19.
+ */
+class TraderActivity : AppCompatActivity() {
+
+    private lateinit var fab: FloatingActionButton
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_trader)
+
+        fab = findViewById(R.id.fab)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, getString(R.string.generating_new_cryptos), Snackbar.LENGTH_SHORT)
+                .setAction("Info", null).show()
+        }
+
+    }
+
+    fun showGeneralServerErrorMessage() {
+        Snackbar.make(fab, getString(R.string.error_while_connecting_to_the_server), Snackbar.LENGTH_LONG)
+            .setAction("Info", null).show()
+    }
+}
